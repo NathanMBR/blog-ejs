096239e996101467efac602e8e9ae46761a4b572
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-06-21 11:52:15 -0300

    Minor code edits

--- db/connect-ion.js
+++ db/connect-ion.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("dbName", "username", "password", {
     host: "localhost",
     dialect: "mysql",
-    timezone: "-05:00"
+    timezone: "-05:00",
+	logging: false
 });
 
 module.exports = connection;
